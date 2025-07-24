d7b1a8df902d617c5f86cbecc3dd9636b483b04b
Devanshi Dave <intern@cricheroes.com>
2025-07-24 18:16:31 +0530

    added procfile

--- app.py
+++ app.py
@@ -92,5 +92,5 @@ def index():
     return render_template("index.html", recs=recs)
 
 if __name__ == "__main__":
-    app.run(debug=False, use_reloader=False)
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
